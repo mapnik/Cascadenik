a4381be06c8496cc457a443413e3fe250384b6a9
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2008-11-16 23:18:59 +0000

    First (untested) draft of cascadenik setuptools config
    git-svn-id: https://mapnik-utils.googlecode.com/svn/trunk/serverside/cascadenik@324 dbea3d40-3153-0410-b551-57805770c6e6

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+readme = file('README.txt','rb').read()
+
+setup(name='cascadenik',
+        version = 0.1.0,
+        #py_modules = ['cascadenik.compile', 'cascadenik.style']
+        description='Cascading Stylesheets For Mapnik',
+        long_description=readme,
+        author='Michal Migurski',
+        author_email='mike@teczno.com',
+        platforms='OS Independent',
+        license='todo',
+        requires=['Mapnik','cssutils'],
+        #test_suite = 'tests.run_doc_tests',
+        keywords='Mapnik,xml,css,mapping',
+        url='http://mapnik-utils.googlecode.com/',
+        classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Utilities'
+        ],
+        #scripts = ['nik2img.py'],
+        #packages=['niktests'],
+        packages=find_packages(exclude=[]),
+        zip_safe=False,
+        )
