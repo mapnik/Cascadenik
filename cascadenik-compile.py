ed59ee6461374ded0bdc78b5014e2aba88f307b8
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2009-03-01 18:30:29 +0000

    Add some basic error checking in options parsing
    git-svn-id: https://mapnik-utils.googlecode.com/svn/trunk/serverside/cascadenik@577 dbea3d40-3153-0410-b551-57805770c6e6

--- cascadenik-compile.py
+++ cascadenik-compile.py
@@ -10,12 +10,16 @@ def main(file, dir):
     print cascadenik.compile(file, dir)
     return 0
 
-parser = optparse.OptionParser(usage="""compile.py [options] <style file>""")
+parser = optparse.OptionParser(usage="""cascadenik-compile.py [options] <style file>""")
 
 parser.add_option('-d', '--dir', dest='directory',
                   help='Write to output directory')
 
 if __name__ == '__main__':
     (options, args) = parser.parse_args()
+    if not args:
+        parser.error('Please specify a .mml file')
     layersfile = args[0]
+    if layersfile.endswith('.mss'):
+        parser.error('Only accepts an .mml file')
     sys.exit(main(layersfile, options.directory))
