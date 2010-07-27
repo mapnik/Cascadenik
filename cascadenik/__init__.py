c97e3f3a86cb5d2c2de73ce2c2db8fb45742feba
dane.springmeyer <dane.springmeyer@dbea3d40-3153-0410-b551-57805770c6e6>
2010-07-27 23:44:11 +0000

    touch up __init__.py adding doc string, skipping mapnik2 support for now, and setting up for ability to pass kwargs to compile
    git-svn-id: https://mapnik-utils.googlecode.com/svn/trunk/serverside/cascadenik@1009 dbea3d40-3153-0410-b551-57805770c6e6

--- cascadenik/__init__.py
+++ cascadenik/__init__.py
@@ -1,3 +1,11 @@
+""" cascadnik - cascading sheets of style for mapnik
+
+http://mike.teczno.com/notes/cascadenik.html
+
+http://code.google.com/p/mapnik-utils/wiki/Cascadenik
+
+"""
+
 import os, tempfile
 import style
 # compile module
@@ -6,17 +14,25 @@ import compile as _compile
 from compile import compile
 from style import stylesheet_rulesets, rulesets_declarations, stylesheet_declarations
 
-try:
-    import mapnik
-except ImportError:
-    # it's not going to be possible to use load_map(), but nothing else requires mapnik
-    pass
+# define Cascadenik version
+VERSION = '0.2.0'
 
-def load_map(map, input):
+__all__ = ['compile','_compile','style','stylesheet_rulesets', 'rulesets_declarations', 'stylesheet_declarations']
+
+def load_map(map, input,**kwargs):
     """
     """
+#    try:
+#        import mapnik2 as mapnik
+#    except ImportError:
+    try:
+        import mapnik
+    except ImportError:
+        # it's not going to be possible to use load_map(), but nothing else requires mapnik
+        pass
+    
     (handle, compiled) = tempfile.mkstemp('.xml', 'cascadenik-compiled-')
     os.close(handle)
-
-    open(compiled, 'w').write(compile(input))
+    
+    open(compiled, 'w').write(compile(input,**kwargs))
     return mapnik.load_map(map, compiled)
