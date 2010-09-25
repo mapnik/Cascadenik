d8dedccc5d3dfed1fd72fa7414d274afb61a2608
migurski <migurski@ubuntu.(none)>
2010-09-25 12:13:23 -0700

    Made output XML file compulsory and removed targget_dir option as a result (might add it back in later)

--- cascadenik-compile.py
+++ cascadenik-compile.py
@@ -7,6 +7,8 @@ import cascadenik
 import tempfile
 import mapnik
 
+from os.path import realpath, dirname
+
 try:
     import xml.etree.ElementTree as ElementTree
     from xml.etree.ElementTree import Element
@@ -18,7 +20,7 @@ except ImportError:
         import elementtree.ElementTree as ElementTree
         from elementtree.ElementTree import Element
 
-def main(file, **kwargs):
+def main(src_file, dest_file, **kwargs):
     """ Given an input layers file and a directory, print the compiled
         XML file to stdout and save any encountered external image files
         to the named directory.
@@ -26,83 +28,77 @@ def main(file, **kwargs):
     mmap = mapnik.Map(1, 1)
     # allow [zoom] filters to work
     mmap.srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null'
-    load_kwargs = dict([(k, v) for (k, v) in kwargs.items() if k in ('target_dir', 'move_local_files')])
-    cascadenik.load_map(mmap, file, **load_kwargs)
+    load_kwargs = {'move_local_files': kwargs.get('move_local_files', None),
+                   'target_dir': dirname(realpath(dest_file))}
+    cascadenik.load_map(mmap, src_file, **load_kwargs)
     
-    (handle, filename) = tempfile.mkstemp(suffix='.xml', prefix='cascadenik-mapnik-')
+    (handle, tmp_file) = tempfile.mkstemp(suffix='.xml', prefix='cascadenik-mapnik-')
     os.close(handle)
-    mapnik.save_map(mmap, filename)
+    mapnik.save_map(mmap, tmp_file)
     
     if kwargs.get('pretty'):
-        doc = ElementTree.fromstring(open(filename, 'rb').read())
+        doc = ElementTree.fromstring(open(tmp_file, 'rb').read())
         cascadenik._compile.indent(doc)
-        f = open(filename, 'wb')
+        f = open(tmp_file, 'wb')
         ElementTree.ElementTree(doc).write(f)
         f.close()
         
-    if kwargs.get('compiled'):
-        os.rename(filename, kwargs['compiled'])
-    else:
-        print open(filename, 'r').read()
-        os.unlink(filename)
+    os.rename(tmp_file, dest_file)
     return 0
 
 parser = optparse.OptionParser(usage="""%prog [options] <mml> <xml>""", version='%prog ' + cascadenik.VERSION)
 
-parser.add_option('-d', '--dir', dest='target_dir',
-                  default=None,
-                  help='Write file-based resources (symbols, shapefiles, etc) to this target directory (default: temp directory is used)')
+parser.set_defaults(move_local_files=False, no_cache=False, pretty=True, safe_urls=False, verbose=False)
+
+#parser.add_option('-d', '--dir', dest='target_dir',
+#                  help='Write file-based resources (symbols, shapefiles, etc) to this target directory (default: current working directory is used)')
 
 parser.add_option('--srs', dest='srs',
-                  default=None,
                   help='Target srs for the compiled stylesheet. If provided, overrides default map srs in the mml (default: None)')
 
 parser.add_option('--move', dest='move_local_files',
-                  default=False,
                   help='Move local files to target --dir location in addition to remote resources (default: False)',
                   action='store_true')
 
 parser.add_option('--no-cache', dest='no_cache',
-                  default=False,
                   help='Do not cache remote files (caching on by default, e.g. no_cache=True)',
                   action='store_true')
 
 parser.add_option('-p', '--pretty', dest='pretty',
-                  default=True,
                   help='Pretty print the xml output (default: True)',
                   action='store_true')
 
 parser.add_option('--safe-urls', dest='safe_urls',
-                  default=False,
                   help='Base64 encode all urls when saved as filesystem paths (default: False)',
                   action='store_true')
 
 parser.add_option('-v' , '--verbose', dest='verbose',
-                  default=False,
                   help='Make a bunch of noise (default: False)',
                   action='store_true')
 
 parser.add_option('--mapnik-version',dest='mapnik_version_string',
-                  default=None,
                   help='The Mapnik version to target (default is 0.7.1 if not able to be autodetected)')
 
 if __name__ == '__main__':
     (options, args) = parser.parse_args()
 
     if not args:
-        parser.error('Please specify a .mml file')
+        parser.error('Please specify .mml and .xml files')
 
-    layersfile = args[0]
+    layersfile, outputfile = args[0:2]
+    
+    print >> sys.stderr, 'output file:', outputfile, dirname(realpath(outputfile))
 
-    if layersfile.endswith('.mss'):
-        parser.error('Only accepts an .mml file')
-    if len(args) == 2:
-        options.compiled = args[1]
-        if not options.compiled.endswith('xml'):
-           parser.error('Stylesheet Output "%s" must end with an ".xml" extension' % options.compiled)
+    if not layersfile.endswith('.mml'):
+        parser.error('Input must be an .mml file')
+
+    if not outputfile.endswith('.xml'):
+        parser.error('Output must be an .xml file')
+
     if options.mapnik_version_string:
         n = options.mapnik_version_string.split('.')
         if not len(n) == 3:
            parser.error('--mapnik-release is invalid, please provide major.minor.point format (e.g. 0.7.1)')
         options.mapnik_version = (int(n[0]) * 100000) + (int(n[1]) * 100) + (int(n[2]));
-    sys.exit(main(layersfile, **options.__dict__))
+
+    sys.exit(main(layersfile, outputfile, **options.__dict__))
