ee07298a7c84d4e5faf8d9c6c7c7b9d8629e9ce2
migurski <migurski@ubuntu.(none)>
2010-09-25 18:39:05 -0700

    Made resolve_paths() because the path-resolution parts of uri will be useful elsewhere

--- cascadenik/style.py
+++ cascadenik/style.py
@@ -22,36 +22,39 @@ class color:
 class color_transparent(color):
     pass
 
+def resolve_paths(address, base):
+    """
+    """
+    addr_scheme, n, addr_path, p, q, f = urlparse.urlparse(address)
+    base_scheme, n, base_path, p, q, f = urlparse.urlparse(base)
+    
+    base_scheme = base_scheme or 'file'
+    base_path = base_path.startswith('/') and base_path or os.path.realpath(base_path)
+
+    if addr_scheme:
+        # fully-qualified
+        return address
+    
+    elif base_scheme == 'http':
+        # remote - urljoin will know what to do in all cases
+        return urlparse.urljoin(base, address)
+    
+    elif base_scheme == 'file' and addr_path.startswith('/'):
+        # local, absolute
+        return 'file://' + addr_path
+    
+    elif base_scheme == 'file' and not addr_path.startswith('/'):
+        # local, relative
+        return os.path.relpath(addr_path, os.path.dirname(base_path))
+
+    else:
+        # can this be reached?
+        raise Exception('Not sure what to do with uri: %s relative to %s' % (address, base))
+
 class uri:
     def __init__(self, address, base=''):
         # sometimes comes in as None
-        base = base or ''
-    
-        addr_scheme, n, addr_path, p, q, f = urlparse.urlparse(address)
-        base_scheme, n, base_path, p, q, f = urlparse.urlparse(base)
-        
-        base_scheme = base_scheme or 'file'
-        base_path = base_path.startswith('/') and base_path or os.path.realpath(base_path)
-
-        if addr_scheme:
-            # fully-qualified
-            self.address = address
-        
-        elif base_scheme == 'http':
-            # remote - urljoin will know what to do in all cases
-            self.address = urlparse.urljoin(base, address)
-        
-        elif base_scheme == 'file' and addr_path.startswith('/'):
-            # local, absolute
-            self.address = 'file://' + addr_path
-        
-        elif base_scheme == 'file' and not addr_path.startswith('/'):
-            # local, relative
-            self.address = os.path.relpath(addr_path, os.path.dirname(base_path))
-
-        else:
-            # can this be reached?
-            raise Exception('Not sure what to do with uri: %s relative to %s' % (address, base))
+        self.address = resolve_paths(address, base or '')
 
     def __repr__(self):
         return str(self.address) #'url("%(address)s")' % self.__dict__
