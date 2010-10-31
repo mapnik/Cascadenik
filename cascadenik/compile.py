6807572f62922ce4a5d84c3d41bfb2b7d214e844
Dane Springmeyer <dane@dbsgeo.com>
2010-10-31 13:16:08 -0700

    monkey with 41b4cc so that the import does not fail since port, strict, and timeout were undefined

--- cascadenik/compile.py
+++ cascadenik/compile.py
@@ -26,8 +26,9 @@ else:
     from httplib import HTTPConnection as _HTTPConnection
     import socket
     
-    def HTTPConnection(host, port=port, strict=strict, timeout=timeout):
-        socket.setdefaulttimeout(timeout)
+    def HTTPConnection(host, port=None, strict=None, timeout=None):
+        if timeout:
+            socket.setdefaulttimeout(timeout)
         return _HTTPConnection(host, port=port, strict=strict)
 
 # cascadenik
