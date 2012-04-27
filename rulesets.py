8d67380af1997733890e444eb846dab21c67ab99
Michal Migurski <mike@stamen.com>
2012-04-26 22:40:20 -0700

    Added a heap of dummy classes and organized the main loop, somewhat

--- rulesets.py
+++ rulesets.py
@@ -1,6 +1,47 @@
 from cssutils.tokenize2 import Tokenizer as cssTokenizer
 
+#
+# Dummy classes.
+#
+
 class ParseException (Exception): pass
+
+class Dummy:
+    def __str__(self):
+        return str(self.__class__) \
+             + '(' + ', '.join( [k+'='+repr(v) for (k, v) in self.__dict__.items()] ) + ')'
+
+class Declaration (Dummy):
+    def __init__(self, selector, property, value, sort_key):
+        self.selector = selector
+        self.property = property
+        self.value = value
+        self.sort_key = sort_key
+
+class Selector (Dummy):
+    def __init__(self, *elements):
+        self.elements = elements[:]
+
+class SelectorElement (Dummy):
+    def __init__(self, names=None, tests=None):
+        self.names = names or []
+        self.tests = tests or []
+
+class SelectorAttributeTest (Dummy):
+    def __init__(self, property, op, value):
+        assert op in ('<', '<=', '=', '!=', '>=', '>')
+        self.op = op
+        self.property = str(property)
+        self.value = value
+
+class Property (Dummy):
+    def __init__(self, name):
+        self.name = name
+
+class Value (Dummy):
+    def __init__(self, value, important):
+        self.value = value
+        self.important = important
 
 css = """
 
@@ -36,7 +77,7 @@ def post_attribute(tokens):
         if nname == 'IDENT':
             print '  A.', value
         
-        elif (nname, value) in [('CHAR', '<'), ('CHAR', '>')]:
+        elif (nname, value) in [('CHAR', '<'), ('CHAR', '>'), ('CHAR', '!')]:
             _nname, _value, line, col = tokens.next()
 
             if (_nname, _value) == ('CHAR', '='):
@@ -95,33 +136,36 @@ while True:
     
     if nname == 'IDENT':
         print '1.', nname, repr(value)
-    
-    elif nname == 'HASH':
-        print '2.', nname, repr(value)
-    
-    elif (nname, value) == ('CHAR', '.'):
         
         while True:
             nname, value, line, col = tokens.next()
             
-            if nname == 'IDENT':
-                print '3.', nname, repr(value)
-                break
+            if nname == 'HASH':
+                print '2.', nname, repr(value)
             
-            else:
-                # something other than a class?
-                raise Exception()
-    
-    elif (nname, value) == ('CHAR', '*'):
-        print '3.', nname, repr(value)
-    
-    elif (nname, value) == ('CHAR', ','):
-        print '- or'
-    
-    elif (nname, value) == ('CHAR', '['):
-        post_attribute(tokens)
-    
-    elif (nname, value) == ('CHAR', '{'):
-        print '- end selector'
-        post_block(tokens)
-        print '-' * 20
+            elif (nname, value) == ('CHAR', '.'):
+                
+                while True:
+                    nname, value, line, col = tokens.next()
+                    
+                    if nname == 'IDENT':
+                        print '3.', nname, repr(value)
+                        break
+                    
+                    else:
+                        # something other than a class?
+                        raise Exception()
+            
+            elif (nname, value) == ('CHAR', '*'):
+                print '3.', nname, repr(value)
+            
+            elif (nname, value) == ('CHAR', ','):
+                print '- or'
+            
+            elif (nname, value) == ('CHAR', '['):
+                post_attribute(tokens)
+            
+            elif (nname, value) == ('CHAR', '{'):
+                print '- end selector'
+                post_block(tokens)
+                print '-' * 20
